3b877fb6942cc829df063c1f68474825d4574cf7
Maxim baginskiy <kachan1208@gmail.com>
2018-03-03 16:44:10 +0200

    Fix

--- gtranslate.go
+++ gtranslate.go
@@ -3,20 +3,20 @@ package gtranslate
 import (
     "net/http"
     "net/url"
-    "fmt"
     "io/ioutil"
     "regexp"
 )
 
 const (
-    //Fix for RU-UA/UA-RU translation(wrong encoding)
     BaseUrl   = "https://translate.googleapis.com/translate_a/single"
+    
+    //Fix for RU-UA/UA-RU translation(wrong encoding)
     UserAgent = "Mozilla/5.0 (X11; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0" 
+    resultExp = regexp.MustCompile(`\[\[\["([^,]+)",`)
 )
 
 var (
     client = &http.Client{}
-    resultExp = regexp.MustCompile(`\[\[\["([^,]+)",`)
 )
 
 func Translate(text string, sourceLang string, targetLang string) (string, error) {
@@ -29,8 +29,7 @@ func Translate(text string, sourceLang string, targetLang string) (string, error
     params.Set("tl", targetLang)
     params.Set("q", text)
 
-    api.RawQuery = params.Encode()
-    fmt.Println(api.String())    
+    api.RawQuery = params.Encode()    
 
     req, err := http.NewRequest("GET", api.String(), nil)
     if err != nil {
